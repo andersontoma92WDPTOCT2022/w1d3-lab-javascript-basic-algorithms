db4b55c7d773c2410a542f7af6c56a2d83295aec
andersontoma92WDPTOCT2022 <115597537+andersontoma92WDPTOCT2022@users.noreply.github.com>
2022-10-19 10:34:42 -0300

    done! ex 1,2,3

--- js/index.js
+++ js/index.js
@@ -1,6 +1,65 @@
 // Iteration 1: Names and Input
-//
+/*
+### Iteration 1: Names and Input
+
+  1.1 Create a variable `hacker1` with the driver's name. <br>
+  1.2 Print `"The driver's name is XXXX"`.<br>
+  1.3 Create a variable `hacker2` with the navigator's name.<br>
+  1.4 Print `"The navigator's name is YYYY"`.
+*/
+let hacker1 = 'driversName';
+let hacker2 = 'navigatorsName';
+console.log(`The driver's name is ${hacker1}`);
+console.log(`The navigator's name is ${hacker2}`);
+// ------------------------------------
 // Iteration 2: Conditionals
+/*
+### Iteration 2: Conditionals
+  2.1. Depending on which name [is longer](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/length), print:
+  <br>
+	  - `The driver has the longest name, it has XX characters.` or <br>
+	  - `It seems that the navigator has the longest name, it has XX characters.` or <br>
+	  - `Wow, you both have equally long names, XX characters!`.
+*/
+if (hacker1.length > hacker2.length) {
+  console.log(
+    `The driver has the longest name, it has ${hacker1.length} characters.`
+  );
+} else if (hacker1.length < hacker2.length) {
+  console.log(
+    `It seems that the navigator has the longest name, it has ${hacker2.length} characters.`
+  );
+} else {
+  console.log(
+    `Wow, you both have equally long names, ${hacker1.length} characters!`
+  );
+}
 
+/*Iteration 3: Loops
+### Iteration 3: Loops
+  3.1 Print all the characters of the driver's name, separated by a space and [in capitals](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase)
+  i.e. `"J O H N"`
+  
+  3.2 Print all the characters of the navigator's name, in reverse order. 
+  i.e. `"nhoJ"`
+  
+  3.3 Depending on the [lexicographic order](https://en.wikipedia.org/wiki/Lexicographical_order) of the strings, print: <br>
+    - `The driver's name goes first.` <br>
+    - `Yo, the navigator goes first definitely.` <br>
+    - `What?! You both have the same name?`*/
+const upperString = [];
+for (let i = 0; i < hacker1.length; i++) {
+  upperString.push(hacker1[i]);
+  if (hacker1.length !== i + 1) {
+    upperString.push(' ');
+  }
+}
+console.log(upperString);
 
-// Iteration 3: Loops
+/*
+#### Bonus 1:
+Go to [lorem ipsum generator](http://www.lipsum.com/) and:
+  - Generate 3 paragraphs. Store the text in a variable type of string.
+  - Make your program count the number of words in the string.
+  - Make your program count the number of times the Latin word [`et`](https://en.wiktionary.org/wiki/et#Latin) appears.
+  */
